a6b1f9baf21fc5e96a2b2798be1a29705f8082ba
RAGHAV GUPTA <149306772+guptaraghav81@users.noreply.github.com>
2024-02-01 18:36:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,273 @@
+word_dict={
+    "Accomplish":"achieve or complete successfully",
+    "Attain":"succeed in achieving",
+    "Adapt": "make something suitable for a new use or purpose",
+    "Affordable":"reasonably priced",
+    "Accompany":"go somewhere with someone as a companion",
+    "Analyse":" examine methodically and in detail",
+    "Admire":"regard an object quality or person with respect or warm approval",
+    "Associate":"connect someone or something with something else in one’s mind",
+    "Artificial":"made by human beings rather than occurring naturally",
+    "Apparently":"as far as one knows or can see",
+    "Beside":"at the side of next to",
+    "Balance":"an even distribution of weight enabling someone or something to remain upright and steady",
+    "Benefit":"an advantage or profit gained from something",
+    "Boundary":"a  line  that  marks  the  limits  of  an  area",
+    "Borrow ":"take  and  use  something  that  belongs  to  someone  else  with  the  intention  of  returning  it",
+    "Better":"of  a  more  excellent  or  effective  type  or  quality",
+    "Babysitting":"the  care  of  a  child  or  children  while  the  parents  are  out",
+    "Backup":"help  or  support",
+    "Backdrop":"provide  a  background  or  setting  for",
+    "Bucketlist":"a  number  of  experiences  or  achievements  that  a  person  hopes  to  have  or  accomplish  during  their  lifetime",
+    "Charge":"demand  an  amount  as  a  price  from  someone  for  a  service  rendered  or  goods  supplied",
+    "Categorise":"place  in  a  particular  class  or  group",
+    "Calculate":"determine  the  amount  or  number  of  something  mathematically",
+    "Conclusion":"the  end  or  finish  of  an  event  or  process",
+    "Contrast":"the  state  of  being  striking  ly  different  from  something  else  in  juxtaposition  or  close  association",
+    "Collide":"hit  with  force  when  moving",
+    "Confuse":"cause  someone  to  become  bewildered  or  perplexed",
+    "Cordially":"in  a  warm  and  friendly  way",
+    "Chase":"pursue  in  order  to  catch  or  catch  up  with",
+    "Cast":"register",
+    "Conference":"a  formal  meeting  for  discussion",
+    "Challenge":"a  call  to  take  part  in  a  contest  or  competition  especially  a  duel",
+    "Dare":"have  the  courage  to  do  something",
+    "Delete":"remove  or  obliterate",
+    "Defend":"resist  an  attack  made  on  someone  or  something",
+    "Detect":"  discover  or  identify  the  presence  or  existence  of",
+    "Detach":"leave  or  separate  oneself  from",
+    "Destroy":" put  an  end  to  the  existence  of  something  by  damaging  or  attacking  it",
+    "Differentiate":"recognize  or  ascertain  what  makes  someone  or  something  different",
+    "Distinguish":"recognize  or  treat  someone  or  something  as  different",
+    "Efficient":"achieving  maximum  productivity  with  minimum  wasted  effort  or  expense",
+    "Effective":"successful  in  producing  a  desired  or  intended  result",
+    "Enormous":"very  large  in  size  quantity  or  extent",
+    "Evidence":"the  available  body  of  facts  or  information  indicating  whether  a  belief  or  proposition  is  true  or  valid",
+    "Eliminate":"completely  remove  or  get  rid  of  something,",
+    "Engagement":"an  arrangement  to  do  something  or  go  somewhere  at  a  fixed  time",
+    "Elementary":"relating  to  the  basic  elements  of  a  subject",
+    "Enigmaticv":"difficult  to  interpret  or  understand",
+    "Friction":"conflict  or  animosity  caused  by  a  clash  of  wills  temperaments  or  opinions",
+    "Fraction":"a  small  or  tiny  part  of  something  ",
+    "Function":"an  activity  or  purpose  natural  to  or  intended  for  a  person  or  thing",
+    "Fund":"a  sum  of  money  saved  or  made  available  for  a  particular  purpose",
+    "Frequent":"happening  often  ",
+    "Fable":"a short  story  typically  with  animals  as  characters  conveying  a  moral",
+    "Fabulous":"extraordinary",
+    "antastic":"extraordinarily  good  or  attractive",
+    "Foreign":" belonging  to  a  country  other  than  one’s  own",
+    "General":" affecting  or  concerning  all  or  most  people  places  or  things",
+    "Generous":"showing  kindness  toward  others",
+    "Gratitude":"the  quality  of  being  thankful",
+    "Grateful":"feeling  or  showing  an  appreciation  of  kindness",
+    "Genetic":" relating  to  genes  or  heredity",
+    "Gigantic":"of  very  great  size",
+    "Gaze":"look  steadily  and  intently",
+    "Game":"a  competitive  form  of  play  played  according  to  rules  and  decided  by  skill  strength  or  luck",
+    "Gorgeous":"very  attractive",
+    "Genre":"a  style  or  category  of  art  music  or  literature",
+    "Hurry":"move  or  act  with  great  speed",
+    "Holy":"dedicated  to  God  or  a  religious  purpose ",
+    "Hallucination":"an  experience  involving  the  apparent  perception  of  something  not  present",
+    "Heavenly":"of  heaven  divine",
+    "Harmony":" the  quality  of  forming  a  pleasing  and  consistent  whole",
+    "Hilarious":"extremely  amusing",
+    "Heartfelt":"sincere",
+    "Harvest":" the  product  or  result  of  an  action",
+    "Humanitarian":"concerned with  or  seeking  to  promote  human  welfare",
+    "Highlight":"an  outstanding  part  of  an  event  or  period  of  time",
+    "Illuminate":"make  something  visible  or  bright  by  shining  light  on  it",
+    "Illustrate":"provide  with  pictures",
+    "Imagination":"the  action  of  forming  new  ideas  or  images  or  concepts  of  external  objects  not  present  to  the  senses",
+    "Inferior":"lower  in  rank  status  or  quality",
+    "Impactful":"having  a  major  effect",
+    "Interior":"relating  to  the  inside",
+    "Improper":"not  in  accordance  with  accepted  rules  or  standards",
+    "Impossible":"not able  to  occur  exist  or  be  done",
+    "Invisible":"unable  to  be  seen",
+    "Incentive":"a  thing  that  motivates  or  encourages  one  to  do  something",
+    "Iconic":"relating  to  or  of  the  nature  of  an  icon",
+    "Jolly":"happy  and  cheerful",
+    "Justice":" just  behaviour  or  treatment",
+    "Judicial":"relating  to  or  done  by  a  court  or  judge",
+    "Judgement":"the  ability  to  make  considered  decisions  or  come  to  sensible  conclusions",
+    "Junior":"denoting  young  or  younger  people",
+    "Knowledge":"the  theoretical  or  practical  understanding  of  a  subject",
+    "Kind":"having  or  showing  a  friendly  generous  and  considerate  nature",
+    "Keen":"having  or  showing  eagerness  or  enthusiasm",
+    "Keep":"have  or  retain  possession  of",
+    "Lavish":"spending  or  using  things  that  are  more  than  necessary",
+    "Luxurious":"extremely  comfortable  elegant  or  enjoyable  especially  in  a  way  that  involves  great  expense",
+    "Long-lasting":"enduring  or  having  endured  for  a  long  period  of  time",
+    "Lovable":"inspiring  or  deserving  love  or  affection",
+    "Late":"doing  something  or  taking  place  after  the  expected  proper  or  usual  time",
+    "Linguistic":"relating  to  language",
+    "Legend":"an  extremely  famous  or  notorious  person  especially  in  a  particular  field",
+    "Loot":"goods  especially  private  property  taken  from  an  enemy  in  war",
+    "Loud":"producing  or  capable  of  producing  much  noise",
+    "Luminous":"giving  off  light",
+    "Levitate":"rise  or  cause  to  rise  and  hover  in  the  air  especially  by  means  of  supernatural  or  magical  power",
+    "Leave":"go  away  from",
+    "Marginal":"relating  to  or  situated  at  the  edge  of  something",
+    "Majestic":"having  or  showing  impressive  beauty",
+    "Magnetic":"the  ability  to  attract  substances  possessing  a  magnetic  quality  (eg:  iron)",
+    "Masculine":"having  qualities  or  appearance  traditionally  associated  with  men",
+    "Maternal":"relating  to  a  mother",
+    "Minimal":" of  a  minimum  amount  quantity,  or  degree",
+    "Manufacturing":"producing  something  on  a  large  scale  using  machinery",
+    "Materialistic":"excessively  concerned  with  material  possessions",
+    "Neighbour":"a  person  living  near  or  next  door",
+    "Native":"  associated  with  the  place  or  circumstances  of  a  person’s  birth",
+    "Narration":"the  action  or  process  of  narrating  a  story",
+    "Normal":"  the  usual  or  typical  state  or  condition",
+    "National":"relating  to  a  nation",
+    "Optimistic":"hopeful  and  confident  about  the  future",
+    "Optional":"available  to  be  chosen  but  not  obligatory",
+    "Objective":"something  that  is  planned  aimed  at  or  sought",
+    "Ordinary":"Without  any  special  or  distinctive  features",
+    "Opportunity":"	a  set  of  circumstances  that  makes  it  possible  to  do  something",
+    "Practical":"concerned  with  the  actual  doing  or  use  of  something  rather  than  with  theory  and  ideas",
+    "Publish":"prepare and issue for  public  sale  distribution  or  readership",
+    "Permit":"authorization or consent to someone to do something",
+    "Ultimately":"in the end",
+    "Victory":"an act of defeating an enemy or opponent in a battle"
+    }
+stages = ['''
+  +------+
+  |      |
+  O      |
+ /|\     |
+ / \     |
+         |
+=========
+''', '''
+  +-----+
+  |     |
+  O     |
+ /|\    |
+ /      |
+        |
+=========
+''', '''
+  +------+
+  |      |
+  O      |
+ /|\     |
+         |
+         |
+=========
+''', '''
+  +-------+
+  |       |
+  O       |
+ /|       |
+          |
+          |
+=========''', '''
+  +-----+
+  |     |
+  O     |
+  |     |
+        |
+        |
+=========
+''', '''
+  +-----+
+  |     |
+  O     |
+        |
+        |
+        |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+      _                                             
+    | |                                            
+    | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+    | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+    | | | | (_| | | | | (_| | | | | | | (_| | | | |
+    |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                        __/ |                      
+                       |___/    '''
+
+
+a1='''
+          ________
+       \ \  / /   / /    \ \  | |      | |  | |    | |  ||  |\\   ||
+        \ \/ /   | |      | | | |      | |  | |    | |  ||  ||\\  ||
+         |  |    | |      | | | |      | |  | | /\ | |  ||  || \\ ||
+         |  |     \ \_____/ /  \ \_____/ /  |__/  \__|  ||  ||  \\||
+
+you win
+'''
+
+import os
+import random
+
+
+
+chosen_word = random.choice(list(word_dict)).lower()  # random.choice(word_dic)
+value = word_dict[chosen_word.capitalize()]
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+
+print(logo)
+
+# Testing code
+print("\n", "the meaning of the word :", value, "\n", "number of alphabets in the word :", word_length)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+print(str(display))
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    os.system('cls')
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            display[position] = letter
+            print(f"You guessed {guess}, that's  in the word. You guessed it right.")
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+
+
+        print(a1)
+        break
+
+
+
+    print(stages[lives])
+
+
+
